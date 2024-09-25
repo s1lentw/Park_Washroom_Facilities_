98d4e265b82c2dd23669c2d6627a01db947ca075
s1lentw <165117809+s1lentw@users.noreply.github.com>
2024-09-25 13:28:23 -0400

    Add files via upload

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Simulates... [...UPDATE THIS...]
+# Author: Haoan Wang
+# Date: September 24 2024
+# Contact: haoanmartin.wang@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Simulate data ####
+# [...ADD CODE HERE...]
+
+
+
---
+++ scripts/01-download_data.R
@@ -0,0 +1,26 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Author: Haoan Wang
+# Date: September 24 2024
+# Contact: haoanmartin.wang@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(opendatatoronto)
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Download data ####
+# [...ADD CODE HERE TO DOWNLOAD...]
+
+
+
+#### Save data ####
+# [...UPDATE THIS...]
+# change the_raw_data to whatever name you assigned when you downloaded it.
+write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+
+         
